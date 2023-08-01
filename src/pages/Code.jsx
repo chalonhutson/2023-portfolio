f5a3bb72ea27010dad277915b4ed127e5290f067
Chalon Hutson <chalonhutson@gmail.com>
2023-08-01 14:23:46 -0600

    Update link for whip it.

--- src/pages/Code.jsx
+++ src/pages/Code.jsx
@@ -5,9 +5,9 @@ export default function Code() {
         <div>
             <h1>Code</h1>
             <div>
-                <h2>Whip It</h2>
-                <p>Tech: React front-end, Flask/Flask-SQLAlchemy back-end</p>
-                <Link to="http://www.whip-it.us" target="_blank" rel="noopener noreferrer">
+                <h2>Whip It (concept)</h2>
+                <p>Tech: React front-end, Flask/Flask-SQLAlchemy back-end, Postgresql database</p>
+                <Link to="https://www.whip-it.us" target="_blank" rel="noopener noreferrer">
                     <button>Visit Whip It</button>
                 </Link>
             </div>
@@ -20,7 +20,7 @@ export default function Code() {
             </div>
             <div>
                 <h2>Easy CRM</h2>
-                <p>Tech: Flask/Flask-SQLAlchemy</p>
+                <p>Tech: Flask/Flask-SQLAlchemy, Postgresql</p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/b7jIAu68G-A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <Link to="https://easy-crm.us" target="_blank" rel="noopener noreferrer">
                     <button>Visit Easy CRM</button>
