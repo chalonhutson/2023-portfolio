660bfd51f8c995c129cea7a51737d2d658dea482
Jessica Swenson <jessicamswens@gmail.com>
2023-08-03 19:44:13 -0600

    added cyberpunk theme on nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import './assets/css/Navbar.css'
 
 export default function Navbar() {
     return (
-        <>
-            <h1>Chalon Hutson</h1>
-            <h2>Software Developer</h2>
-            <div>
-                <Link to="/"><button className="navBtn">Home</button></Link>
-                <Link to="/code"><button className="navBtn">Code</button></Link>
-                <Link to="/3d-art"><button className="navBtn">3d Art</button></Link>
-                <Link to="/game-development"><button className="navBtn">Game Development</button></Link>
-                <Link to="/contact"><button className="navBtn">Contact</button></Link>
+        <div className="navbar-div">
+            <div className="banner-div">
+                <h1 className="c-name glow">Chalon Hutson</h1>
+                <h2 className="title glow-blue">Software Developer</h2>
             </div>
-        </>
+            <div className="nav-btn-container">
+                <Link className="link-css" to="/"><button className="card">Home</button></Link>
+                <Link className="link-css" to="/code"><button className="card">Code</button></Link>
+                <Link className="link-css" to="/3d-art"><button className="card">3d Art</button></Link>
+                <Link to="/game-development"><button className="card">Game Development</button></Link>
+                <Link className="link-css" to="/contact"><button className="card">Contact</button></Link>
+            </div>
+        </div>
     )
 }
