ca681ab02fa6dcaba010a1ab1f74aa943f5879da
Jessica Swenson <jessicamswens@gmail.com>
2023-08-04 08:58:50 -0600

    updated bkg color and opc bkg img

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
                 <Link className="link-css" to="/"><button className="card">Home</button></Link>
                 <Link className="link-css" to="/code"><button className="card">Code</button></Link>
                 <Link className="link-css" to="/3d-art"><button className="card">3d Art</button></Link>
-                <Link to="/game-development"><button className="card">Game Development</button></Link>
+                <Link className="link-css" to="/game-development"><button className="card">Game Development</button></Link>
                 <Link className="link-css" to="/contact"><button className="card">Contact</button></Link>
             </div>
         </div>
