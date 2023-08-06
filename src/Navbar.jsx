7b5144b817962322fe4194406049ba866036c729
Jessica Swenson <jessicamswens@gmail.com>
2023-08-05 23:39:39 -0600

    added background img for btns

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
                 <h2 className="title glow-blue">Software Developer</h2>
             </div>
             <div className="nav-btn-container">
-                <Link className="link-css" to="/"><button className="card">Home</button></Link>
-                <Link className="link-css" to="/code"><button className="card">Code</button></Link>
-                <Link className="link-css" to="/3d-art"><button className="card">3d Art</button></Link>
+                <Link className="link-css home-link" to="/"><button className="card home-btn">Home</button></Link>
+                <Link className="link-css" to="/code"><button className="card code-btn">Code</button></Link>
+                <Link className="link-css" to="/3d-art"><button className="card d-art-btn">3d Art</button></Link>
                 <Link className="link-css" to="/game-development"><button className="card">Game Development</button></Link>
                 <Link className="link-css" to="/contact"><button className="card">Contact</button></Link>
             </div>
